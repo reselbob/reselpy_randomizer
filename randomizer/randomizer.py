bdfd55b180ac752383f601f96e35334ee15c001b
Bob Reselman <breselman@h2wellness.com>
2017-03-16 16:31:17 -0700

    Code style clean up

--- randomizer/randomizer.py
+++ randomizer/randomizer.py
@@ -1,6 +1,7 @@
 import json
 import random
 import os.path
+import string
 
 class Randomizer:
 
@@ -8,58 +9,62 @@ class Randomizer:
         path  = os.path.join(os.path.dirname(__file__), json_file_spec)
         with open(path) as json_data:
             d = json.load(json_data)
-            json_data.close()
-            return d;
+            return d
     secure_random = random.SystemRandom()
     cities = json_loader('data/cities.js')
     first_names = json_loader('data/first_names.js')
     last_names = json_loader('data/last_names.js')
     words = json_loader('data/words.js')
     extensions = ['com', 'net', 'org', 'io', 'tv']
-    alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+'
+    alphabet = string.ascii_lowercase + string.digits + '!@#$%^&*()_+'
 
     @staticmethod
     def get_random_city():
         return Randomizer.secure_random.choice(Randomizer.cities)
+    
     @staticmethod
     def get_random_first_name():
         return Randomizer.secure_random.choice(Randomizer.first_names)
+    
     @staticmethod
     def get_random_last_name():
         return Randomizer.secure_random.choice(Randomizer.last_names)
+    
     @staticmethod
     def get_random_email(first_name, last_name):
         company = Randomizer.get_random_phrase(1)
         ext = Randomizer.secure_random.choice(Randomizer.extensions)
         return '{}.{}@{}.{}'.format(first_name, last_name, company, ext)
+    
     @staticmethod
     def get_random_string(string_length):
-        str = ''
+        random_string = ''
         count = 0
-        while (count < string_length):
+        while count < string_length:
             s = Randomizer.alphabet[random.randint(0, len(Randomizer.alphabet)-1)]
-            if( random.randint(1, 1000) % 2 == 0):
+            if random.randint(1, 1000) % 2 == 0:
                 s = s.upper()
-            str = str + s
+            random_string = random_string + s
             count += 1
-        return str
+        return random_string
+
+    @staticmethod
+    def get_random_int(minimum, maximum):
+        return random.randint(minimum, maximum)
+    
+    @staticmethod
+    def get_random_float(minimum, maximum):
+        return random.uniform(minimum, maximum)
+
     @staticmethod
     def get_random_phrase(number_of_words):
         words = ''
         count = 0
-        while (count < number_of_words):
+        while count < number_of_words:
             word = Randomizer.secure_random.choice(Randomizer.words)
-            if(count == 0):
+            if count == 0:
                 words = word
             else:
-                words += ' {}'.format(word);
+                words += ' {}'.format(word)
             count += 1
         return words
-    @staticmethod
-    def get_random_int(min, max):
-        return random.randint(min, max)
-    @staticmethod
-    def get_random_float(min, max):
-        return random.uniform(min, max)
-
-
