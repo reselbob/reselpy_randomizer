cff48d611e1059b74b507af7e350a722ff1ddbc9
Bob Reselman <breselman@h2wellness.com>
2017-03-21 16:36:16 -0700

    Modified setup with data location information

--- setup.py
+++ setup.py
@@ -5,13 +5,15 @@ from setuptools import setup
 
 setup(
     name='reselpy_randomizer',
-    version='0.3',
+    version='0.4',
     description="A simple project that provides methods for the return random data",
     author="Bob Reselman",
     author_email='reselbob@gmail.com',
     url='https://github.com/reselbob/reselpy_randomizer',
-    download_url='https://github.com/reselbob/reselpy_randomizer/archive/0.3.tar.gz',
+    download_url='https://github.com/reselbob/reselpy_randomizer/archive/0.4.tar.gz',
     packages=['reselpy_randomizer'],
+    package_dir={'reselpy_randomizer': 'reselpy_randomizer'},
+    package_data={'reselpy_randomizer': ['data/*.js']},
     include_package_data=True,
     license="MIT license",
     keywords=['reselpy_randomizer', 'randomizer', 'randomization', 'random'],
