62bc49b3739d1c238998e65732b931d6792c9535
Bob Reselman <breselman@h2wellness.com>
2017-03-21 16:00:42 -0700

    Adjusted setup

--- setup.py
+++ setup.py
@@ -13,12 +13,12 @@ with open('HISTORY.rst') as history_file:
 
 setup(
     name='reselpy_randomizer',
-    version='0.1',
+    version='0.2',
     description="A simple project that provides methods for the return random data",
     author="Bob Reselman",
     author_email='reselbob@gmail.com',
     url='https://github.com/reselbob/reselpy_randomizer',
-    download_url='https://github.com/reselbob/reselpy_randomizer/archive/0.1.tar.gz',
+    download_url='https://github.com/reselbob/reselpy_randomizer/archive/0.2.tar.gz',
     packages=['reselpy_randomizer'],
     include_package_data=True,
     license="MIT license",
