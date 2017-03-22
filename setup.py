e1b5f64310355a497d8eb17408ad930021553ab2
Bob Reselman <breselman@h2wellness.com>
2017-03-21 17:13:53 -0700

    Ticked up setup

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup
 
 setup(
     name='reselpy_randomizer',
-    version='0.4',
+    version='0.5',
     description="A simple project that provides methods for the return random data",
     author="Bob Reselman",
     author_email='reselbob@gmail.com',
     url='https://github.com/reselbob/reselpy_randomizer',
-    download_url='https://github.com/reselbob/reselpy_randomizer/archive/0.4.tar.gz',
+    download_url='https://github.com/reselbob/reselpy_randomizer/archive/0.5.tar.gz',
     packages=['reselpy_randomizer'],
     package_dir={'reselpy_randomizer': 'reselpy_randomizer'},
     package_data={'reselpy_randomizer': ['data/*.js']},
